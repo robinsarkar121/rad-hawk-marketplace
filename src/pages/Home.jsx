8ca9fb6f9d8953f965dde2a946da3e7090e9162b
ThinhinaneZadi <zaditinhinane@gmail.com>
2026-06-08 18:24:45 -0400

    Update homepage and remove categories navigation

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,11 @@
 import Hero from "../components/Hero";
 import CategorySection from "../components/CategorySection";
-import FeaturedItems from "../components/FeaturedItems";
 
 export default function Home() {
   return (
     <div style={{ fontFamily: "sans-serif", minHeight: "100vh", background: "#fff" }}>
       <Hero />
       <CategorySection />
-      <FeaturedItems />
     </div>
   );
 }
